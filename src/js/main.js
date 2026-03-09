c8bf5e56b639d568fcf1ace69c19f0a60f150928
Virginia Dächer <virginiadaecher@gmail.com>
2026-03-09 10:18:25 +0100

    adds navigation

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,19 @@ const userLang = navigator.language.startsWith('en') ? 'en' : 'de';
 loadTranslations(userLang);
 
 window.setLanguage = (lang) => loadTranslations(lang);
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const mobileNav = document.getElementById("mobileNav");
+  const openBtn = document.getElementById("openMenu");
+  const closeBtn = document.getElementById("closeMenu");
+
+  openBtn.addEventListener("click", () => {
+    mobileNav.classList.add("active");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    mobileNav.classList.remove("active");
+  });
+
+});
